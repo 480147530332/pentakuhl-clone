34f1dd22539c40915db8979fb052a43d29fba2c9
SumitMore_ <moresumiit562@gmail.com>
2026-05-28 16:02:02 -0500

    Fix component import paths for production build

--- src/assets/pages/Pentakuhl.jsx
+++ src/assets/pages/Pentakuhl.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import heroVideo from "../video/hero.mp4";
 import PentAbout from "../components/PentAbout";
 
-import SeriesGuide from "../components/SeriesGuide";
+import SeriesGuide from "../components/Seriesguide";
 import Faq from "../components/Faq";
 import Featuresticker from "../components/Featuresticker";
 import Cta from "../components/Cta";
